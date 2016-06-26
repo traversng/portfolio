a7733f9782dfd53e1543f5be05d17aead2613f9e
Travis Ueki <travis.ueki@learningfuze.com>
2016-06-25 17:08:19 -0700

    edited details

---
+++ resources/views/social_links.blade.php
@@ -0,0 +1,6 @@
+<ul class="social-list">
+    <li><a href="#"><span class="fa fa-github"></span></a></li>
+    <li><a href="#"><span class="fa fa-linkedin"></span></a></li>
+    <li><a href="#"><span class="fa fa-google-plus"></span></a></li>
+    <li><a href="#"><span class="fa fa-twitter"></span></a></li>
+</ul>
